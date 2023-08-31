96bfa870c1528d4fff681086182720d0db330120
Hunter Bertoson <dev@hunterbertoson.tech>
2023-08-31 10:08:22 -0500

    Updates

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
 	setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 	testEnvironment: 'jest-environment-jsdom',
+	reporters: [['github-actions', {silent: false}], 'summary'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
